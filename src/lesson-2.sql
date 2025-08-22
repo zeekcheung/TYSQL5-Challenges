78ad7fe8b251e010c5b29c04b4971a9783c7e3bf
zeekcheung <zeekcheung@outlook.com>
2025-08-22 19:20:00 +0800

    chore: day 1

---
+++ src/lesson-2.sql
@@ -0,0 +1,15 @@
+-- Challenge 1
+SELECT cust_id
+FROM Customers;
+
+-- Challenge 2
+SELECT DISTINCT prod_id
+FROM OrderItems;
+
+-- Challenge 3
+SELECT *
+FROM Customers;
+
+-- Challenge 4
+SELECT cust_id
+FROM Customers;
